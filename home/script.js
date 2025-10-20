d4fa2389ecd66ea8f90ea33d1834b96eda5ae48b
Erick Staub <erickstaubfelipe@gmail.com>
2025-10-20 19:56:35 -0300

    inicio

---
+++ home/script.js
@@ -0,0 +1,10 @@
+function Mesa() {
+    window.location.href = "./mesa/index.html";
+}
+function Pedido() {
+    window.location.href = "./pedido/index.html";
+
+}
+function Cardapio() {
+    window.location.href = "./cardapio/index.html";
+}
