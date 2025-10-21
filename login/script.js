a9b2906c82ccce748315ffd2138a6e7da91f0d5b
Erick Staub <erickstaubfelipe@gmail.com>
2025-10-21 19:50:04 -0300

    splashyard

---
+++ login/script.js
@@ -0,0 +1,30 @@
+const baseUrl = "http://localhost:7082/swagger"
+
+const headers ={
+            "content-type":	"application/json; charset=utf-8"
+        }
+async function getUsers() {
+    const response = await fetch(`${baseUrl}/api/Usuario`)
+    const users = await response.json()
+    const nome = document.querySelector(".nome")
+    const email = document.querySelector(".email")
+    const senha = document.querySelector(".senha")
+    
+}
+getUsers()
+function init() {
+    const form = document.querySelector("form")
+    form.addEventListener("submit",(event)=>{
+        event.preventDefault()
+        // createUser()
+        // updateUser()
+        // removeUser()
+        // openModal()
+        // if()
+    })
+}
+
+function Home() {
+
+    window.location.href = "./home/index.html"
+}
