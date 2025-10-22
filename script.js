353b76830c9176c2c7fe040c584dae1b4e0644ca
Erick Staub <erickstaubfelipe@gmail.com>
2025-10-21 21:40:16 -0300

    splashyard v2 buxa + gigante

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-const baseUrl = "http://localhost:7082"
-
-const headers ={
-            "content-type":	"application/json; charset=utf-8"
-        }
-async function getUsers() {
-    const response = await fetch(`${baseUrl}/api/Usuario`)
-    const users = await response.json()
-
-}
-getUsers()
-
-function init() {
-    const form = document.querySelector("form")
-    form.addEventListener("submit",(event)=>{
-        event.preventDefault()
-        // createUser()
-        // updateUser()
-        // removeUser()
-        // openModal()
-        // if()
-    })
-}
-
-function Home() {
-
-    window.location.href = "./home/index.html"
-}
+    function Login(){
+        window.location.href="../login/index.html"
+    }
