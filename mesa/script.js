579dec854e33c37791c2e503454807fa4e24f349
Erick Staub <erickstaubfelipe@gmail.com>
2025-11-14 21:57:48 -0300

    mesas 2

--- mesa/script.js
+++ mesa/script.js
@@ -5,8 +5,70 @@ function Mesa() {
     const li = document.createElement('li');
     const img = document.createElement('img');
     const h1 = document.createElement('h1');
-    img.src = "../img/3293741.png"
+    const h2 = document.createElement('h1');
+    var sorte = Math.floor(Math.random() * 4);
+    if (sorte == 0) {
+        img.src = "../img/mesa1.png"
+    }
+    else if (sorte == 1) {
+        img.src = "../img/mesa2.png"
+    }
+    else if (sorte == 2) {
+        img.src = "../img/mesa3.png"
+    }
+    else {
+        img.src = "../img/mesa4.png"
+    }
     h1.innerText = mesa;
-    li.append(img, h1);
+    h2.innerText = "disponível";
+    li.append(img, h1, h2);
     ul.appendChild(li);
+
 }
+async function Carega() {
+    const baseUrl = "http://localhost:7022";
+    const response = await fetch(`https://localhost:7022/api/Mesa`);
+    const mesas = await response.json();
+    console.log(mesas);
+    for (let i = 0; i < mesas.length; i++) {
+        var mesa = "Mesa " + num++;
+        const ul = document.querySelector('ul');
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h1');
+        var sorte = Math.floor(Math.random() * 4);
+        if (sorte == 0) {
+            img.src = "../img/mesa1.png"
+        }
+        else if (sorte == 1) {
+            img.src = "../img/mesa2.png"
+        }
+        else if (sorte == 2) {
+            img.src = "../img/mesa3.png"
+        }
+        else {
+            img.src = "../img/mesa4.png"
+        }
+        h1.innerText = mesa;
+        var sm = mesas[i].situaçãoMesa;
+        if (sm == 0) {
+            h2.innerText = "Disponível";
+            h2.style.color = "green";
+        }
+        else if (sm == 1) {
+            h2.innerText = "Ocupada";
+            h2.style.color = "red";
+        } else {
+            h2.innerText = "Reservada";
+            h2.style.color = "orange";
+        }
+
+
+        li.append(img, h1, h2);
+        ul.appendChild(li);
+
+    }
+}
+Carega();
+
