4b2d9acf737866fe54a5809884a4207a71257b4f
Erick Staub <erickstaubfelipe@gmail.com>
2025-11-21 20:00:02 -0300

    comanda

--- pedido/script.js
+++ pedido/script.js
@@ -1,12 +1,28 @@
 async function CarregaPedido() {
     const response = await fetch('https://localhost:7022/api/Comanda');
     const data = await response.json();
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    const img = document.createElement('img');
-    const h1 = document.createElement('h1');
-    h1.innerText = `Mesa: ${data.mesaId} \n Cliente: ${data.clienteNome} \n Situação: ${data.situaçãoComanda}`;
-    li.appendChild(h1);
-    ul.appendChild(li);
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        const ul = document.querySelector('ul');
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h1');
+        var situ = data[i].situaçãoComanda;
+        var txt = '';
+        if (situ == 0) {
+            txt = 'Aberta';
+
+        } else if (situ == 1) {
+            txt = 'Fechada';
+
+        } else {
+            txt = 'Cancelada';
+        }
+        h1.innerText = `Mesa: ${data[i].mesaId} \n Cliente: ${data[i].clienteNome} \n Situação: ${txt}`;
+
+        li.append(h1, h2);
+        ul.appendChild(li);
+    }
 }
 CarregaPedido();
