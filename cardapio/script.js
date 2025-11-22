773d1f2cf160b31ba1187b1eb69f6900257cf942
Erick Staub <erickstaubfelipe@gmail.com>
2025-11-21 21:51:37 -0300

    coisas

--- cardapio/script.js
+++ cardapio/script.js
@@ -3,6 +3,7 @@ async function Carrega() {
     const cardapios = await resposta.json();
     console.log(cardapios);
     for (let i = 0; i < cardapios.length; i++) {
+        let p = 0
         const ul = document.querySelector('ul');
         const li = document.createElement('li');
         const img = document.createElement('img');
@@ -18,13 +19,79 @@ async function Carrega() {
             img.src = "../img/batata.png"
         } else if (cardapios[i].titulo.toLowerCase().includes("hot")) {
             img.src = "../img/hot.png"
+        } else if (cardapios[i].titulo.toLowerCase().includes("merda")) {
+            img.src = "../img/merda.jpg"
         }
+        li.addEventListener('click', () => {
+            if (p == 0) {
+                const detalhe = document.createElement('h2');
+                detalhe.innerText = cardapios[i].descricao;
+                li.appendChild(detalhe);
+                li.style.height = "450px"
+                p = 1;
+            }
+            else if (p == 1) {
+                li.removeChild(li.lastChild);
+                p = 0;
+                li.style.height = "400px"
+            }
 
+
+        });
         li.append(img, h1, h2);
         ul.appendChild(li);
     }
 }
 Carrega();
-function CardapioPost() {
-    console.log("Botão de adicionar cardápio clicado");
+async function CardapioPost() {
+    document.body.insertAdjacentHTML('beforeend', `
+        <div class="wrapper">
+        <div class="modal">
+        <button class="close-button" id="close">X</button>
+                <h1>Adicionar Item ao Cardápio</h1>
+                <form>
+                    <label for="titulo">Título:</label>
+                    <input type="text" id="titulo" name="titulo" required>
+                    <label for="descricao">Descrição:</label>
+                    <input type="text" id="descricao" name="descricao" required>
+                    <label for="preco">Preço:</label>
+                    <input type="number" id="preco" name="preco" step="0.01" required>
+                    <label for="pp">Possui Preparo?</label>
+                    <input type="checkbox" id="pp" name="pp">
+                    <button type="submit" id="add">Adicionar</button>
+                </form>
+        </div>
+    </div>
+    `)
+    const closeButton = document.getElementById('close');
+    closeButton.addEventListener('click', () => {
+        const wrapper = document.querySelector('.wrapper');
+        wrapper.remove()
+    });
+    const add = document.getElementById('add');
+    add.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const titulo = document.getElementById('titulo').value;
+        const descricao = document.getElementById('descricao').value;
+        const preco = parseFloat(document.getElementById('preco').value);
+        const pp = document.getElementById('pp').checked;
+        const body = {
+            titulo: titulo,
+            descricao: descricao,
+            preco: preco,
+            possuiPreparo: pp
+        };
+        const response = await fetch('https://localhost:7022/api/CardapioItem', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }); if (response.ok) {
+            alert('Item adicionado com sucesso!');
+            location.reload();
+        } else {
+            alert('Erro ao adicionar item.');
+        }
+    });
 }
--- mesa/script.js
+++ mesa/script.js
@@ -1,31 +1,31 @@
 var num = 1;
-function Mesa() {
-    var mesa = "Mesa " + num++;
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    const img = document.createElement('img');
-    const h1 = document.createElement('h1');
-    const h2 = document.createElement('h1');
-    var sorte = Math.floor(Math.random() * 4);
-    if (sorte == 0) {
-        img.src = "../img/mesa1.png"
-    }
-    else if (sorte == 1) {
-        img.src = "../img/mesa2.png"
-    }
-    else if (sorte == 2) {
-        img.src = "../img/mesa3.png"
-    }
-    else {
-        img.src = "../img/mesa4.png"
-    }
-    h1.innerText = mesa;
-    h2.innerText = "disponível";
-    h2.style.color = "green";
-    li.append(img, h1, h2);
-    ul.appendChild(li);
+// function Mesa() {
+//     var mesa = "Mesa " + num++;
+//     const ul = document.querySelector('ul');
+//     const li = document.createElement('li');
+//     const img = document.createElement('img');
+//     const h1 = document.createElement('h1');
+//     const h2 = document.createElement('h1');
+//     var sorte = Math.floor(Math.random() * 4);
+//     if (sorte == 0) {
+//         img.src = "../img/mesa1.png"
+//     }
+//     else if (sorte == 1) {
+//         img.src = "../img/mesa2.png"
+//     }
+//     else if (sorte == 2) {
+//         img.src = "../img/mesa3.png"
+//     }
+//     else {
+//         img.src = "../img/mesa4.png"
+//     }
+//     h1.innerText = mesa;
+//     h2.innerText = "disponível";
+//     h2.style.color = "green";
+//     li.append(img, h1, h2);
+//     ul.appendChild(li);
 
-}
+// }
 async function Carega() {
     const baseUrl = "http://localhost:7022";
     const response = await fetch(`https://localhost:7022/api/Mesa`);
@@ -65,6 +65,32 @@ async function Carega() {
             h2.style.color = "orange";
         }
 
+        let a = 0;
+        li.addEventListener('click', () => {
+            if (a == 0) {
+                li.style.height = "450px";
+                const dis = document.createElement('button')
+                const ocu = document.createElement('button')
+                const res = document.createElement('button')
+                dis.innerText = "Disponível"
+                ocu.innerText = "Ocupada"
+                res.innerText = "Reservada"
+                li.append(dis);
+                li.append(ocu);
+                li.append(res);
+                a = 1;
+
+            } else {
+                li.style.height = "400px";
+                li.removeChild(li.lastChild);
+                li.removeChild(li.lastChild);
+                li.removeChild(li.lastChild);
+                a = 0;
+            }
+            console.log(a);
+
+
+        });
 
         li.append(img, h1, h2);
         ul.appendChild(li);
@@ -77,6 +103,15 @@ async function Mesapost() {
     var mesa = "Mesa " + nummesa;
     const ul = document.querySelector('ul');
     const li = document.createElement('li');
+    li.addEventListener('click', () => {
+        let sm = 0;
+        if (sm == 0) {
+            li.style.backgroundColor = "lightgreen";
+        }
+
+
+
+    });
     const img = document.createElement('img');
     const h1 = document.createElement('h1');
     const h2 = document.createElement('h1');
@@ -118,4 +153,5 @@ async function Mesapost() {
 
 
 }
+
 
