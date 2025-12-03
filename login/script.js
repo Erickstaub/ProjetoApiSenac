55b93de15f3dcba38bedd431eb1df222cc225b05
LORENZZOANTONIOPATAT <LORENZZOANTONIOPATAT@SCS217758>
2025-12-02 21:59:21 -0300

    login transformando com post

--- login/script.js
+++ login/script.js
@@ -1,29 +1,31 @@
 function Home() {
-
     window.location.href = "./home/index.html"
 }
-// const baseUrl = "http://localhost:7082/swagger"
 
-// const headers = {
-//     "content-type": "application/json; charset=utf-8"
-// }
-// async function getUsers() {
-//     const response = await fetch(`${baseUrl}/api/Usuario`)
-//     const users = await response.json()
-//     const nome = document.querySelector(".nome")
-//     const email = document.querySelector(".email")
-//     const senha = document.querySelector(".senha")
 
-// }
-// getUsers()
-// function init() {
-//     const form = document.querySelector("form")
-//     form.addEventListener("submit", (event) => {
-//         event.preventDefault()
-//         // createUser()
-//         // updateUser()
-//         // removeUser()
-//         // openModal()
-//         // if()
-//     })
-// }
+async function Login() {
+
+    const emailInput = document.querySelector('#email');
+    const senhaInput = document.querySelector('#senha');
+    const user = {
+        email:emailInput.value,
+        senha:senhaInput.value
+    }
+        const response = await fetch(`https://localhost:7022/api/Usuario/Login`,{
+            method:"POST",
+            body:JSON.stringify(user),
+             headers: {
+                        'Content-Type': 'application/json'
+                    },
+        });
+        if(response.ok){
+            Home();
+        }else{
+            //abrir modal informando que deu errado
+        }
+        //const res = await response.json()
+
+}
+
+// se o login deu certo
+//Home();
