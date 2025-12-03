2969281838218774aabd296e4a9cd19558ecf862
Erick Staub <erickstaubfelipe@gmail.com>
2025-12-02 21:57:43 -0300

    funciona 1/2

--- funcionario/script.js
+++ funcionario/script.js
@@ -20,6 +20,58 @@ async function Carrega() {
         } else if (funcao.textContent == "gerente") {
             img.src = "/img/gerente.png"
         }
+        li.addEventListener("click", async () => {
+            document.body.insertAdjacentHTML('beforeend', `
+                <div class="wrapper">
+                <div class="modal">
+                <button class="close-button" id="close">X</button>
+                <h1 id="h1"></h1>
+                <br>
+
+                <form>
+               <h1>Cargo:</h1>
+                <br>
+                <label>
+  <input type="radio" name="cargo" value="gerente" required>
+  Gerente
+</label>
+                <br>
+
+<label>
+  <input type="radio" name="cargo" value="cozinheiro">
+  Cozinheiro
+</label>
+                <br>
+
+<label>
+  <input type="radio" name="cargo" value="garcon">
+  Garçom
+</label>
+                <br>
+
+<label>
+  <input type="radio" name="cargo" value="atendente">
+  Atendente
+</label>
+                <br>
+
+                <h2>novo salario:</h2>
+                
+
+                <input type="number" id="number" name="salario" required>
+                <br>
+
+                <button type="submit" id="done">Salvar</button>
+                </form>
+                </div>
+                </div>
+            `)
+            const h1 = document.getElementById("h1")
+            const salario = document.getElementById("salario")
+            h1.innerText = usuario[i].nome + "\n" + usuario[i].email
+
+
+        })
         li.append(funcao, img, nome, salario)
         ul.append(li)
 
