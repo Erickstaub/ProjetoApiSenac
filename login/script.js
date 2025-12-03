3334b643057ca94c862ec49bc8357b4790920ab0
LORENZZOANTONIOPATAT <LORENZZOANTONIOPATAT@SCS217758>
2025-12-03 19:25:28 -0300

    login

--- login/script.js
+++ login/script.js
@@ -1,3 +1,5 @@
+import { baseUrl } from "../baseUrl";
+
 function Home() {
     window.location.href = "./home/index.html"
 }
@@ -11,7 +13,7 @@ async function Login() {
         email:emailInput.value,
         senha:senhaInput.value
     }
-        const response = await fetch(`https://localhost:7022/api/Usuario/Login`,{
+        const response = await fetch(`${baseUrl}/api/Usuario/Login`,{
             method:"POST",
             body:JSON.stringify(user),
              headers: {
@@ -22,6 +24,23 @@ async function Login() {
             Home();
         }else{
             //abrir modal informando que deu errado
+            document.body.insertAdjacentHTML('beforeend', `
+        <div class="reswrapper">
+        <div class="resmodal">
+        <button class="close-button" id="close">X</button>
+        <h1>Cadastre a sua Reserva!</h1>
+        <form>
+        <label>Nome:</label>
+            <input type="text" id="nome" name="nome" required>
+            <label>Telefone:</label>
+            <input type="text" id="telefone" name="telefone" required>
+            <label>Data:</label>
+            <input type="date" id="date" name="data" required>
+            <button type="submit" id="done">RESERVAR</button>
+        </form>
+        </div>
+        </div>
+        `)
         }
         //const res = await response.json()
 
